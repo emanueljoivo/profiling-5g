96b970be2343c57c2e7359b22286fe8c2fee3149
Joívo <emanueljoivo@gmail.com>
2019-12-13 07:56:40 -0300

    Add data folder

--- first_scenario/tcp_server.sh
+++ first_scenario/tcp_server.sh
@@ -6,4 +6,6 @@ OUTPUT_FILE="$(date +%s)".tcp.csv
 
 iperf --server --enhanced | tee "${OUTPUT_FILE}"
 
+mkdir -p data/
+
 mv "${OUTPUT_FILE}" data/
--- first_scenario/udp_server.sh
+++ first_scenario/udp_server.sh
@@ -6,4 +6,6 @@ OUTPUT_FILE="$(date +%s)".udp.csv
 
 iperf --server --enhanced --udp | tee "${OUTPUT_FILE}"
 
+mkdir -p data/
+
 mv "${OUTPUT_FILE}" data/
