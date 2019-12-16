d9062419cf384637ab743fdb0e3efcb9538576fb
Joívo <emanueljoivo@gmail.com>
2019-12-16 12:03:32 -0300

    Fix data file name and netperf cmd improves

--- second_scenario/udp_client.sh
+++ second_scenario/udp_client.sh
@@ -2,5 +2,5 @@
 
 set -o xtrace
 
-iperf --client 189.40.186.130 --enhanced \
-  --udp --tradeoff --bandwidth 1200M --time 60 --interval 1
+iperf --udp --client 189.40.186.130 --enhanced \
+  --udp --tradeoff --bandwidth 1200M --time 600
