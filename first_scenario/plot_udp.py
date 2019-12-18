22ed50b5493b4e0caedd610e00c1ad05ff0f9a07
Emanuel Joívo <emanueljoivo@gmail.com>
2019-12-18 09:45:37 -0300

    Fix csv references

--- first_scenario/plot_udp.py
+++ first_scenario/plot_udp.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
 import csv
 
+def to_mega(b):
+    return b / (10**6)
+
 intervals = []
 data_transfer = []
 bandwidth = []
-
-def to_mega(b):
-    return b / (10**6)
 
 with open('data/udp_upload.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
 
     for row in csv_reader:
-        bandwidth.append(to_mega(int(row[-1])))
-        data_transfer.append(to_mega(int(row[-2])))
-        intervals.append(row[-3])
+        bandwidth.append(to_mega(int(row[-6])))
+        data_transfer.append(to_mega(int(row[-7])))
+        intervals.append(row[-8])
 
 results = [intervals.pop(), data_transfer.pop(), bandwidth.pop()]
 intervals = list(map(lambda interval: float(interval.split("-")[-1]), intervals))
