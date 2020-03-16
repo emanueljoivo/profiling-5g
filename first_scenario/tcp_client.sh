7020af83bd88522c44410301db07f5140bb06d8f
Emanuel Joívo <emanueljoivo@gmail.com>
2020-03-16 14:48:00 -0300

    Add data

--- first_scenario/tcp_client.sh
+++ first_scenario/tcp_client.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace
 
-OUTPUT_FILE=tcp_download-mi-5g-pro-16032020.csv
+OUTPUT_FILE=data/tcp_download-mi-5g-pro-16032020.csv
 
 # the test will execute for ten minutes
-iperf --client 189.40.186.4 --time 300 --interval 1 --reportstyle C --output "${OUTPUT_FILE}"
+iperf --client 189.40.186.4 --time 300 --interval 1 --reportstyle C | tee "${OUTPUT_FILE}"
